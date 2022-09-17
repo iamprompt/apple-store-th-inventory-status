c4517f9cfe5d65eb35c463e3ba0a95d3be435b9c
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-09-18 02:12:24 +0700

    add responsive

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ const App = () => {
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-5 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
         {data.items
           .sort(
             (a, b) =>
